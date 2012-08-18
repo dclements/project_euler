e91eab9537dc869a5b4e4789ce055d6ac1b929d2
dclements <dclemen@mac.com>
2012-08-18 16:00:05 -0600

    Adding rudimentary support for JScience Rationals.

---
+++ scala/src/main/scala/dclements/pe/Rational.scala
@@ -0,0 +1,92 @@
+package dclements.pe;
+
+import scala.math._
+import org.jscience.mathematics.number.{Rational => JSRational, LargeInteger}
+
+object Rational {
+  def apply(num: LargeInt, den: LargeInt=LargeInt.One) =
+    new Rational(JSRational.valueOf(num.largeInteger, den.largeInteger))
+  
+  implicit def int2rational(i: Int): Rational = apply(i)
+  implicit def long2rational(i: Long): Rational = apply(i)
+  implicit def lageInt2rational(i: LargeInt): Rational =
+    apply(i)
+  implicit def bigInt2rational(i: BigInt): Rational =
+    apply(i)
+  
+  trait RationalOrdering extends Ordering[Rational] {
+    def compare(x: Rational, y: Rational) = x.compare(y)
+  }
+  implicit object Rational extends RationalOrdering
+  
+  lazy val MinusOne = apply(-1)
+  lazy val One = apply(1)
+  lazy val OneFifth = apply(1, 5)
+  lazy val OneHalf = apply(1, 2)
+  lazy val OneQuarter = apply(1, 4)
+  lazy val OneThird = apply(1, 3)
+  lazy val Zero = apply(0)
+  lazy val TwoThirds = apply(2, 3)
+}
+
+class Rational(val rational: JSRational) extends ScalaNumber with ScalaNumericConversions with Serializable {
+  override def hashCode(): Int = rational.##
+  
+  protected def isWhole = divisor == 1
+  def underlying = rational
+    
+  override def equals(that: Any): Boolean = that match {
+    case that: Rational => this equals that
+    case that: LargeInt => this equals Rational(that, LargeInt.One)
+    case x => isValidLong && unifiedPrimitiveEquals(x)
+  }
+  
+  override def isValidByte = isWhole && this >= Byte.MinValue && this <= Byte.MaxValue
+  override def isValidShort = isWhole && this >= Short.MinValue && this <= Short.MaxValue
+  override def isValidChar = isWhole && this >= Char.MinValue && this <= Char.MaxValue
+  override def isValidInt = isWhole && this >= Int.MinValue && this <= Int.MaxValue
+  def isValidLong = isWhole && this >= (Long.MinValue + 1) && this <= (Long.MaxValue - 1)
+  
+  def dividend: LargeInt = new LargeInt(rational.getDividend())
+  def divisor: LargeInt = new LargeInt(rational.getDivisor())
+  
+  def equals(that: Rational): Boolean = compare(that) == 0
+  def compare(that: Rational): Int = this.rational.compareTo(that.rational)
+  
+  def doubleValue = this.rational.doubleValue
+  def floatValue = this.rational.floatValue
+  def longValue = this.rational.longValue
+  def intValue = this.rational.intValue
+  override def byteValue = this.rational.byteValue
+  
+  def <= (that: Rational): Boolean = compare(that) <= 0
+  def >= (that: Rational): Boolean = compare(that) >= 0
+  
+  def < (that: Rational): Boolean = compare(that) < 0
+  def > (that: Rational): Boolean = compare(that) > 0
+  
+  def + (that: Rational): Rational = new Rational(this.rational.plus(that.rational))
+  def - (that: Rational): Rational = new Rational(this.rational.minus(that.rational))
+  def unary_- : Rational = this.opposite()
+  def * (that: Rational): Rational = new Rational(this.rational.times(that.rational))
+  def / (that: Rational): Rational = new Rational(this.rational.divide(that.rational))
+  
+  def abs: Rational = new Rational(this.rational.abs())
+  
+  def isLargerThan(i: Rational): Boolean = this.rational.isLargerThan(i.rational)
+  
+  def isNegative: Boolean = this.rational.isNegative()
+  
+  def isPositive: Boolean = this.rational.isPositive()
+  
+  def isZero: Boolean = this.rational.isZero()
+  
+  def min(that: Rational): Rational = if (this < that) this else that
+  def max(that: Rational): Rational = if (this < that) that else this
+  
+  def pow(exp: Int): Rational = new Rational(this.rational.pow(exp))
+  
+  def opposite(): Rational = new Rational(this.rational.opposite())
+  
+  override def toString(): String = this.rational.toString()
+}
---
+++ scala/src/test/scala/dclements/pe/RationalSpec.scala
@@ -0,0 +1,71 @@
+package dclements.pe;
+
+import org.scalatest.FunSpec
+import collection.immutable.TreeMap
+
+
+class RationalSpec extends FunSpec {
+  
+  describe("A Rational number") {
+    
+    it("should convert from a BigInt") {
+      val i: Rational = BigInt(1)
+      
+      assert(i == 1)
+    }
+    
+    it("should convert from a LargeInt") {
+      val i: Rational = LargeInt(1)
+      
+      assert(i == 1)
+    }
+    
+    it("should convert from a Long") {
+      val i: Rational = 1
+      
+      assert(i == 1)
+    }
+    
+    it("should equal integers of the same size") {
+      assert(5 == Rational(5))
+    }
+    
+    it("should not equal integers of a different size") {
+      assert(!(2 == Rational(5)))
+    }
+    
+    it("should compare greater than") {
+      expect(1) {
+        LargeInt(5).compare(LargeInt(2))
+      }
+    }
+    
+    it("should compare less than") {
+      expect(-1) {
+        Rational(2,3).compare(Rational(3,2))
+      }
+    }
+    
+    it("should compare equals") {
+      expect(0) {
+        Rational(2,3).compare(Rational(2,3))
+      }
+    }
+    
+    it("should take a unary minus") {
+      expect(Rational(-1)) {
+        -Rational(1)
+      }
+    }
+  }
+  
+  describe("A LargeIntOrdering") {
+    it("should sort correctly") {
+      val m = TreeMap[Rational, Int](Rational(2, 3)->1, Rational(3,2)->1, Rational(0)->1)
+      
+      expect(List[Rational](Rational(0), Rational(2, 3), Rational(3, 2))) {
+        m.keysIterator.toList
+      }
+    }
+  }
+}
